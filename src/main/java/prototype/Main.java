77468850b81f9667088b1f4c30de02e5521813d8
Samuel Ricardo Quintero Agudelo <squinteroagudelo18@gmail.com>
2023-09-27 23:09:56 -0500

    manual implementation

--- src/main/java/prototype/Main.java
+++ src/main/java/prototype/Main.java
@@ -2,6 +2,15 @@ package prototype;
 
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.brand = "Ferrari";
+        car.model = "488 GTB";
+        car.doors = 2;
 
+        System.out.println("car: brand = " + car.brand + ", model = " + car.model + ", doors = " + car.doors);
+
+        Car clone = car.clone();
+        clone.model = "Another model";
+        System.out.println("car: brand = " + clone.brand + ", model = " + clone.model + ", doors = " + clone.doors);
     }
 }
